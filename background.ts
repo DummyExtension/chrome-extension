ef33660c17ce5f825f05bb8ac631187345757765
Dummy EXT <dummyextension365@gmail.com>
2024-10-29 22:45:30 -0600

    Adding async API call

--- background.ts
+++ background.ts
@@ -1,3 +1,11 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  sendResponse('Hello extension!');
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  const result = await fetch('https://swapi.dev/api/people/1', {method: 'GET'});
+
+  if (result.ok) {
+    const data = await result.json();
+    sendResponse(`Hello ${data?.name}!`);
+    return true;
+  }
+  
+  sendResponse('There was an issue calling the API!');
 });
